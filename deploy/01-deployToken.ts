cf850250cf19788b2bcbb36a4b3313a49b245f9a
fbrcode <fabio.bressler@gmail.com>
2022-07-21 09:14:06 -0300

    perform contract deployment

--- deploy/01-deployToken.ts
+++ deploy/01-deployToken.ts
@@ -1,11 +1,18 @@
 import { DeployFunction } from 'hardhat-deploy/types';
-import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types'; // from ./node_modules/hardhat-deploy/types.ts
 
 const deployToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const contractName = 'OurToken';
-  const { deployments, getNamedAccounts, network, ethers } = hre;
+  const { deployments, getNamedAccounts, network } = hre;
   const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
   log(`Deploying ERC20 Token "${contractName}" on network: ${network.name}`);
+  const token = await deploy(contractName, {
+    from: deployer, // deployer is the default account
+    args: [1000000], // 1 million tokens as initial supply
+    log: true, // log the transaction
+    waitConfirmations: 1, // wait for 1 block confirmation
+  });
   log(`Token "${contractName}" Deployed!`);
   log('----------------------------------------------------------');
 };
