da9f8133be9a510ff606b36278e859034b6b2552
fbrcode <fabio.bressler@gmail.com>
2022-07-21 09:12:33 -0300

    declare depoyer named account

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,6 +14,12 @@ const config: HardhatUserConfig = {
   typechain: {
     outDir: './artifacts/typechain-types',
   },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  },
 };
 
 export default config;
